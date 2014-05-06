a23eb4f69397817289c7fd549235ced04eb0e797
Nick Overdijk <nick@astrant.net>
2014-05-06 15:40:16 +0200

    Handle mingw-c++11

--- enable_cpp11.cmake
+++ enable_cpp11.cmake
@@ -1,6 +1,10 @@
-
+if(MINGW)
+	include(${CMAKE_CURRENT_LIST_DIR}/enable_gnu11.cmake)
+else()
 #TODO: Fix this, use Dakon's detect C++11 module git://anongit.kde.org/scratch/dakon/cmake-cxx11
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-if(APPLE)
-	set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -stdlib=libc++")
-endif(APPLE)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+	#TODO: Is this still necessary on OSX 10.9?
+	if(APPLE)
+		set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -stdlib=libc++")
+	endif(APPLE)
+endif()
