2d770bd22fcc53bde895a6c255ff7b4e4aa25ff0
Nick Overdijk <nick@astrant.net>
2014-06-20 16:40:43 +0200

    Adds FindAssimp.cmake (missing on Ubuntu)

---
+++ modules/FindAssimp.cmake
@@ -0,0 +1,15 @@
+# - Try to find Assimp
+# Once done this will define
+#  Assimp_FOUND - system has Assimp
+#  Assimp_INCLUDE_DIRS - include directories for Assimp
+#  Assimp_LIBRARIES - libraries for Assimp
+
+FIND_PATH(Assimp_INCLUDE_DIRS mesh.h HINTS /usr/include/assimp/)
+FIND_LIBRARY(Assimp_LIBRARIES NAMES assimp HINTS /usr/lib/)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Assimp DEFAULT_MSG
+                                  Assimp_LIBRARIES Assimp_INCLUDE_DIRS)
+
+set(Assimp_FOUND ${Assimp_FOUND})
+MARK_AS_ADVANCED(Assimp_FOUND Assimp_INCLUDE_DIRS Assimp_LIBRARIES)
