8a91f7febc6441f0b405ba108f9a82608928fbbd
Nick Overdijk <nick@astrant.net>
2014-06-22 02:31:42 +0200

    Changes FindAssimp -- again

--- modules/FindAssimp.cmake
+++ modules/FindAssimp.cmake
@@ -4,10 +4,13 @@
 #  Assimp_INCLUDE_DIRS - include directories for Assimp
 #  Assimp_LIBRARIES - libraries for Assimp
 
-FIND_PATH(Assimp_INCLUDE_DIRS mesh.h HINTS /usr/include/assimp/)
-FIND_LIBRARY(Assimp_LIBRARIES NAMES assimp HINTS /usr/lib/)
+find_path(Assimp_INCLUDE_DIR assimp/mesh.h)
+find_library(Assimp_LIBRARY NAMES assimp)
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Assimp DEFAULT_MSG Assimp_INCLUDE_DIRS Assimp_LIBRARIES)
-set(Assimp_FOUND ${ASSIMP_FOUND})
-MARK_AS_ADVANCED(Assimp_FOUND Assimp_INCLUDE_DIRS Assimp_LIBRARIES)
+set(Assimp_INCLUDE_DIRS ${Assimp_INCLUDE_DIR})
+set(Assimp_LIBRARIES ${Assimp_LIBRARY})
+
+include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+find_package_handle_standard_args(Assimp REQUIRED_VARS Assimp_INCLUDE_DIR Assimp_LIBRARY)
+mark_as_advanced(Assimp_INCLUDE_DIR Assimp_LIBRARY)
+
