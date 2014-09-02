43f8889e78193bf7949494ac9c3a40faff263863
Nick Overdijk <nick@astrant.net>
2014-09-02 17:05:45 +0200

    [sane_install] Adds CMAKE_CONFIGS type of file, install libraries in lib/project/, shortens variable names

--- sane_install.cmake
+++ sane_install.cmake
@@ -8,7 +8,7 @@
 #
 # This will install the given things in CMAKE_INSTALL_PREFIX
 # - include/hurdur/awesomesauce/lib.hpp
-# - lib/library (.a or .so or .dll or ...)
+# - lib/hurdur/library (.a or .so or .dll or ...)
 # - bin/application
 # - lib/hurdur/cmake/hurdurConfig.cmake
 #
@@ -16,32 +16,32 @@
 #
 # = FAQ
 # == How do I install my archives/libraries/runtimes/includes/cmake-things somewhere else?
-# Set sane_install_{archive,library,runtime,include,cmake}_install_path to something else.
+# Set sane_install_{archive,library,runtime,include,cmake_module}_path to something else.
 #
 # == How do I change the installation directory?
 # Just change CMAKE_INSTALL_PREFIX in your CMake Cache (run ccmake or cmake-gui for easy editing).
+#
+# == How can I install configuration files to a different place?
+# You can't, sorry. If you think this is a wrong decision, feel free to send a pull request/mail with a fix.
 
 include(CMakeParseArguments)
 function(sane_install)
 	set(optionalArgs)
 	set(singleArgs HEADER_DESTINATION EXPORT_GROUP)
-	set(multiArgs TARGETS HEADERS CMAKE_MODULES)
+	set(multiArgs TARGETS HEADERS CMAKE_MODULES CMAKE_CONFIGS)
 	cmake_parse_arguments(sane_install "${optionalArgs}" "${singleArgs}" "${multiArgs}" ${ARGN})
 
-	if(NOT DEFINED sane_install_archive_install_path)
-		set(sane_install_archive_install_path "lib")
+	if(NOT DEFINED sane_install_archive_path)
+		set(sane_install_archive_path "lib/${CMAKE_PROJECT_NAME}")
 	endif()
-	if(NOT DEFINED sane_install_library_install_path)
-		set(sane_install_library_install_path "lib")
+	if(NOT DEFINED sane_install_library_path)
+		set(sane_install_library_path "lib/${CMAKE_PROJECT_NAME}")
 	endif()
-	if(NOT DEFINED sane_install_runtime_install_path)
-		set(sane_install_runtime_install_path "bin")
+	if(NOT DEFINED sane_install_runtime_path)
+		set(sane_install_runtime_path "bin")
 	endif()
-	if(NOT DEFINED sane_install_include_install_path)
-		set(sane_install_include_install_path "include/${CMAKE_PROJECT_NAME}")
-	endif()
-	if(NOT DEFINED sane_install_cmake_install_path)
-		set(sane_install_cmake_install_path "${sane_install_library_install_path}/${CMAKE_PROJECT_NAME}/cmake")
+	if(NOT DEFINED sane_install_include_path)
+		set(sane_install_include_path "include/${CMAKE_PROJECT_NAME}")
 	endif()
 	if(NOT DEFINED sane_install_HEADER_DESTINATION)
 		set(sane_install_HEADER_DESTINATION "")
@@ -49,20 +49,25 @@ function(sane_install)
 	if(NOT DEFINED sane_install_EXPORT_GROUP)
 		set(sane_install_EXPORT_GROUP ${CMAKE_PROJECT_NAME})
 	endif()
+	set(sane_install_cmake_config_path "lib/${CMAKE_PROJECT_NAME}/cmake")
+	if(NOT DEFINED sane_install_cmake_module_path)
+		set(sane_install_cmake_module_path "lib/${CMAKE_PROJECT_NAME}")
+	endif()
 
 	install(
 		TARGETS ${sane_install_TARGETS}
 		EXPORT ${sane_install_EXPORT_GROUP}
-		ARCHIVE DESTINATION ${sane_install_archive_install_path}
-		LIBRARY DESTINATION ${sane_install_library_install_path}
-		RUNTIME DESTINATION ${sane_install_runtime_install_path}
+		ARCHIVE DESTINATION ${sane_install_archive_path}
+		LIBRARY DESTINATION ${sane_install_library_path}
+		RUNTIME DESTINATION ${sane_install_runtime_path}
 	)
 
 
-	set(true_install_dir ${sane_install_include_install_path}/${sane_install_HEADER_DESTINATION})
+	set(true_install_dir ${sane_install_include_path}/${sane_install_HEADER_DESTINATION})
 	install(FILES ${sane_install_HEADERS} DESTINATION ${true_install_dir})
-	install(FILES ${sane_install_CMAKE_MODULES} DESTINATION ${sane_install_cmake_install_path})
+	install(FILES ${sane_install_CMAKE_MODULES} DESTINATION ${sane_install_cmake_module_path})
+	install(FILES ${sane_install_CMAKE_CONFIGS} DESTINATION ${sane_install_cmake_config_path})
 	if(DEFINED sane_install_TARGETS)
-		install(EXPORT ${sane_install_EXPORT_GROUP} FILE "${sane_install_EXPORT_GROUP}Config.cmake" DESTINATION ${sane_install_cmake_install_path})
+		install(EXPORT ${sane_install_EXPORT_GROUP} FILE "${sane_install_EXPORT_GROUP}Config.cmake" DESTINATION ${sane_install_cmake_config_path})
 	endif()
 endfunction(sane_install)
