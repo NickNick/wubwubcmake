4c30928b4a39932e6fb1da2a59a05b373b66ad6b
Nick Overdijk <nick@astrant.net>
2014-10-27 16:59:24 +0100

    [sanitizers] Add memory_sanitizer to sanitizers.cmake, gives warnings instead of errors when two are enabled
    I opt for warnings over FATAL_ERROR since perhaps one day they will be
    both supported, and we check for compatability anyway.

--- sanitizers.cmake
+++ sanitizers.cmake
@@ -1,8 +1,14 @@
-
 include(${CMAKE_CURRENT_LIST_DIR}/address_sanitizer.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/memory_sanitizer.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/thread_sanitizer.cmake)
 
 if(USE_ASAN AND USE_TSAN)
-	message(FATAL_ERROR "Sorry enabling both the thread and address sanitizer is not supported")
+	message(WARNING "Probably can't use ASAN and TSAN at the same time")
+endif()
+if(USE_ASAN AND USE_MSAN)
+	message(WARNING "Probably can't use ASAN and MSAN at the same time")
+endif()
+if(USE_TSAN AND USE_MSAN)
+	message(WARNING "Probably can't use MSAN and TSAN at the same time")
 endif()
 
