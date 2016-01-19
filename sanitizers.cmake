00ed8ba32572ef9683581571eb2fc464c3b2396a
Nick Overdijk <nick@astrant.net>
2016-01-19 20:20:58 +0100

    Adds undefined behavior sanitizer

--- sanitizers.cmake
+++ sanitizers.cmake
@@ -1,6 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/address_sanitizer.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/memory_sanitizer.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/thread_sanitizer.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/undefined_behavior_sanitizer.cmake)
 
 if(USE_ASAN AND USE_TSAN)
 	message(WARNING "Probably can't use ASAN and TSAN at the same time")
---
+++ undefined_behavior_sanitizer.cmake
@@ -0,0 +1,17 @@
+include(${CMAKE_CURRENT_LIST_DIR}/flag_compiles.cmake)
+
+option(USE_UBSAN "Enable Undefined Behavior Sanitizer" OFF)
+
+if(USE_UBSAN)
+	set(flags "-fsanitize=undefined -fno-omit-frame-pointer")
+	flag_compiles(FLAGS ${flags} WORKING_FLAG flag)
+
+	if(flag)
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}")
+		set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${flag}")
+		set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${flag}")
+		set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${flag}")
+	else()
+		message(WARNING "Undefined Behavior Sanitizer requested, but your compiler doesn't support it (could be other compilation flags, such as the address sanitizer)")
+	endif()
+endif()
