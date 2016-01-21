0809e8aaed0cd49ca037f6107c6818c88a886a4d
Sjors Gielen <sjors@sjorsgielen.nl>
2016-01-21 12:55:03 +0100

    Add RelWithAsserts build type if you include rel_with_asserts.

---
+++ rel_with_asserts.cmake
@@ -0,0 +1,27 @@
+# This file adds the RelWithAsserts CMAKE_BUILD_TYPE. It is exactly like
+# RelWithDebInfo, but it also keeps asserts intact by not defining -DNDEBUG.
+
+set(CMAKE_CXX_FLAGS_RELWITHASSERTS
+	"-O2 -g"
+	CACHE STRING "Flags used by the C++ compiler during RelWithAsserts builds."
+	FORCE)
+
+set(CMAKE_C_FLAGS_RELWITHASSERTS
+	"-O2 -g"
+	CACHE STRING "Flags used by the C compiler during RelWithAsserts builds."
+	FORCE)
+
+set(CMAKE_EXE_LINKER_FLAGS_RELWITHASSERTS
+	""
+	CACHE STRING "Flags used for linking binaries during RelWithAsserts builds."
+	FORCE)
+SET(CMAKE_SHARED_LINKER_FLAGS_RELWITHASSERTS
+	""
+	CACHE STRING "Flags used by the shared libraries linker during RelWithAsserts builds."
+	FORCE)
+MARK_AS_ADVANCED(
+	CMAKE_CXX_FLAGS_RELWITHASSERTS
+	CMAKE_C_FLAGS_RELWITHASSERTS
+	CMAKE_EXE_LINKER_FLAGS_RELWITHASSERTS
+	CMAKE_SHARED_LINKER_FLAGS_RELWITHASSERTS
+)
