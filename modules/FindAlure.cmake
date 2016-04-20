c69217162a1410f85e1d22e249f7b20b3714f923
Maurice Bos <m-ou.se@m-ou.se>
2016-04-20 18:25:44 +0200

    Use USE_STATIC_LIBS in FindAlure.cmake.

--- modules/FindAlure.cmake
+++ modules/FindAlure.cmake
@@ -5,10 +5,22 @@
 #  Alure_LIBRARIES - libraries for Alure
 
 find_path(Alure_INCLUDE_DIR NAMES alure.h PATH_SUFFIXES AL OpenAL)
-find_library(Alure_LIBRARY NAMES alure ALURE32 ALURE32-static)
+if(USE_STATIC_LIBS)
+	find_library(Alure_LIBRARY NAMES libalure.a libALURE32.a alure-static ALURE32-static)
+else()
+	find_library(Alure_LIBRARY NAMES alure ALURE32)
+endif()
 
 set(Alure_INCLUDE_DIRS ${Alure_INCLUDE_DIR})
 set(Alure_LIBRARIES ${Alure_LIBRARY})
+
+if(USE_STATIC_LIBS)
+	list(APPEND Alure_LIBRARIES sndfile vorbisenc vorbis ogg FLAC mpg123)
+endif()
+
+if(UNIX)
+	list(APPEND Alure_LIBRARIES pthread dl)
+endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
 find_package_handle_standard_args(Alure REQUIRED_VARS Alure_INCLUDE_DIR Alure_LIBRARY)
